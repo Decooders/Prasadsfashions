5a158b2d46c82ef28c38c55bc6113d267c9c5ed9
Subhani Sayyad <65666841+subhani9@users.noreply.github.com>
2023-09-12 12:29:24 +0530

    addons

---
+++ src/pages/Products.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout/Layout.js";
+
+const Products = () => {
+  return (
+    <Layout title={"PRASADSFASHIONS/ShowNow"}>
+      <h1>Products</h1>
+    </Layout>
+  );
+};
+
+export default Products;
